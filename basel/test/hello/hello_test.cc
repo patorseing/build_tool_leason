3588fbd70017b1df00df4bc64a1ea623584cd51e
Napatchol Thaipanich <patorseing@gmail.com>
2021-02-28 06:30:31 +0700

    bazel with google test

--- basel/test/hello/hello_test.cc
+++ basel/test/hello/hello_test.cc
@@ -1,8 +1,29 @@
 #include "hello.hh"
 #include <cassert>
+#include "gtest/gtest.h"
 
-int main() {
+// case TRUE
+TEST(HelloShould, ReturnHello)
+{
   Hello hello;
-  assert(hello.sayHi() == "Hello bazel");
-  // assert(hello.sayHi() == "Annie");
+  std::string expected = "Hello bazel";
+  std::string actual = hello.sayHi();
+  EXPECT_EQ(expected, actual);
 }
+
+// case FAIL
+TEST(HelloShould2, ReturnHello2)
+{
+  Hello hello;
+  std::string expected = "Hello world";
+  std::string actual = hello.sayHi();
+  EXPECT_EQ(expected, actual) << "ERROR: test error";
+}
+
+// int main()
+// {
+//   Hello hello;
+//   std::string expected = "Hello bazel";
+//   std::string actual = hello.sayHi();
+//   assert(actual == expected);
+// }
